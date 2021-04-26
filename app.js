f1323c21649780feaa1fedf85131214bb9862d3c
estebanfon <rojasfonsecae909@gmail.com>
2021-04-26 14:17:11 -0500

    cambios

---
+++ app.js
@@ -0,0 +1,5 @@
+(function(){
+	var lista = document.getElementById("lista");
+	    tareaInput = document.getElementById("tareaInput");
+		btnNuevaTarea = document.getElementById("btnNuevaTarea");
+}());
